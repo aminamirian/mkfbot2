935fc9d78c432141144af3eb6c8da549213227fe
LegendaryTM <peyman.1379@yahoo.com>
2016-08-18 16:45:16 +0430

    Legendary

---
+++ plugins/mute-all.lua
@@ -0,0 +1,44 @@
+﻿do
+local function pre_process(msg)
+ local hash = 'muteall:'..msg.to.id
+  if redis:get(hash) and msg.to.type == 'channel' and not is_momod(msg)  then
+   delete_msg(msg.id, ok_cb, false)
+       end
+    return msg
+ end
+ 
+local function run(msg, matches)
+ if matches[1] == 'muteall' and is_momod(msg) then
+       local hash = 'muteall:'..msg.to.id
+       if not matches[2] then
+              redis:set(hash, true)
+             return "قفل چت فعال شد!"
+ else
+
+local hour = string.gsub(matches[2], 'h', '')
+ local num1 = tonumber(hour) * 3600
+local minutes = string.gsub(matches[3], 'm', '')
+ local num2 = tonumber(minutes) * 60
+local second = string.gsub(matches[4], 's', '')
+ local num3 = tonumber(second) 
+local num4 = tonumber(num1 + num2 + num3)
+redis:setex(hash, num4, true)
+ return "قفل چت فعال شد تا\n#ساعت "..matches[2].."\n#دقیقه "..matches[3].."\n#ثانیه "..matches[4].."\nدیگر😐\nهم اکنون هر کس #چت کند، پاک می شود."
+ end
+ end
+if matches[1] == 'unmuteall' and is_momod(msg) then
+               local hash = 'muteall:'..msg.to.id
+        redis:del(hash)
+          return "قفل چت غیر فعال شد!"
+  end
+end
+return {
+   patterns = {
+      '^[/!#]([Mm]uteall)$',
+      '^[/!#]([Uu]nmuteall)$',
+   '^[/!#]([Mm]uteall) (.*) (.*) (.*)$',
+ },
+run = run,
+  pre_process = pre_process
+}
+end
---
+++ plugins/version.lua
@@ -0,0 +1,11 @@
+﻿local function run (msg)
+	return "Legendary Bot Version 2.0\nLast Update: 15/08/2016 11:10 PM\n\nEdit by: @All_0r_NothinG\nCreated by: @All_0r_NothinG\nChannel: @Legendary_TM\nIf you need help, you can join Support Group\nhttps://telegram.me/joinchat/DEkBIj1P2F5V6ZSRiw15iA\nGood lock :)"
+end
+
+return {
+	patterns = {
+		"^[Vv]ersion$",
+		"^[!/#][Vv]ersion$",
+	},
+run = run
+}
